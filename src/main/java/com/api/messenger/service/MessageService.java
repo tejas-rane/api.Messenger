f445880eca3d172e40a36ca0e4028ccb649608d9
Tejas Rane <tejas.m.rane@gmail.com>
2017-12-31 14:37:22 -0800

    test data changed

--- src/main/java/com/api/messenger/service/MessageService.java
+++ src/main/java/com/api/messenger/service/MessageService.java
@@ -12,11 +12,11 @@ public class MessageService {
 	public MessageService(){
 		messeges.put(1L, new Message(1, "You can use Messages to send texts, photos, videos, and audio messages ", 
 				 "Tejas"));
-		messeges.put(2L, new Message(1, "If you can't reply to a message from the Lock screen, go to Settings ", 
+		messeges.put(2L, new Message(2, "If you can't reply to a message from the Lock screen, go to Settings ", 
 				 "Ted"));
-		messeges.put(3L, new Message(1, "When you delete a message or conversation, you can't get it back. So make sure that you save important information from your messages.", 
+		messeges.put(3L, new Message(3, "When you delete a message or conversation, you can't get it back. So make sure that you save important information from your messages.", 
 				 "John"));
-		messeges.put(4L, new Message(1, "If you want to keep your messages for a certain amount of time, go to Settings > Messages > Keep Messages. ", 
+		messeges.put(4L, new Message(4, "If you want to keep your messages for a certain amount of time, go to Settings > Messages > Keep Messages. ", 
 				 "Peter"));
 	}
 	public List<Message> getAllMesseges(){
